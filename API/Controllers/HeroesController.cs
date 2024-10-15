eff2317f38adc64e73527bf175c7b302bdf82ca4
Ioni <sami.repo@live.com>
2024-10-15 10:17:22 +0300

    Solution Files

---
+++ API/Controllers/HeroesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+/// <summary>
+/// Controller for Heroes related operations.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class HeroesController : ControllerBase
+{
+    private readonly HeroesJsonParser _parser;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="parser"></param>
+    public HeroesController(HeroesJsonParser parser)
+    {
+        _parser = parser;
+    }
+
+    /// <summary>
+    /// Get all factions from the JSON files.
+    /// </summary>
+    /// <returns>List of factions.</returns>
+    [HttpGet("factions")]
+    public IActionResult GetFactions()
+    {
+        var factions = _parser.ParseFactions();
+        return Ok(factions);
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,39 @@
+using API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddScoped<HeroesJsonParser>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Heroes API", Version = "v1" });
+    c.SchemaGeneratorOptions.SchemaIdSelector = type => type.FullName;
+    // Register schemas automatically
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Heroes API V1");
+        c.RoutePrefix = string.Empty;
+    });
+    
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
